c390013d2098b037f2bfa97749ba58729e516e23
Fabián Villena <villenafabian@gmail.com>
2019-09-28 01:48:19 +0200

    initial commit

---
+++ src/data/wl_data.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import os
+import re
+import json
+
+class WlDataRawLoader:
+    def __init__(self, raw_data_directory):
+        self.filenames = [raw_data_directory + filename for filename in os.listdir(raw_data_directory)]
+    def load_files(self):
+        self.data = pd.DataFrame()
+        for filename in self.filenames:
+            SS = re.search(r'SS(\w+)\.',filename).group(1)
+            current = pd.read_csv(filename, low_memory=False)
+            current['SS'] = SS
+            self.data = self.data.append(current)
+    def generate_report(self,report_destination):
+        self.report = self.data.SS.value_counts().to_dict()
+        self.report['total_count'] = int(self.data.SS.count())
+        with open(report_destination, 'w', encoding='utf-8') as json_file:
+            json.dump(self.report, json_file, indent=2, ensure_ascii=False)
+            
