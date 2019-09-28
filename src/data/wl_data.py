990bc843963e91ca00d0a5990876a3feda58c654
Fabián Villena <villenafabian@gmail.com>
2019-09-28 02:06:22 +0200

    low memory and printing actual file

--- src/data/wl_data.py
+++ src/data/wl_data.py
@@ -10,8 +10,9 @@ class WlDataRawLoader:
     def load_files(self):
         self.data = pd.DataFrame()
         for filename in self.filenames:
+            print(filename)
             SS = re.search(r'SS(\w+)\.',filename).group(1)
-            current = pd.read_csv(filename, low_memory=False)
+            current = pd.read_csv(filename, low_memory=True)
             current['SS'] = SS
             self.data = self.data.append(current)
     def generate_report(self,report_destination):
