760391942c9f9b5570057d4dd04119960e3f89e2
Fabián Villena <villenafabian@gmail.com>
2019-10-01 13:09:33 +0200

    processing all raw wl

--- analyze.py
+++ analyze.py
@@ -3,7 +3,7 @@ from src.data.wl_data import WlDataRawLoader
 from src.data.wl_data import WlDataPreprocessor
 from src.data.wl_data import PsoriasisLabeler
 
-raw_data_loader = WlDataRawLoader('data/raw_sample/')
+raw_data_loader = WlDataRawLoader('data/raw/')
 raw_data_loader.load_files()
 raw_data_loader.data.to_csv('data/interim/raw_data.csv', index=False)
 raw_data_loader.generate_report('reports/raw_data_report.json')
